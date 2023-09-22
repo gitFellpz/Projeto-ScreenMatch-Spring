2f21250bc78cff52be041bca5000ad37ca763ba1
gitFellpz <felipe.adrilima@gmail.com>
2023-09-22 17:17:07 -0300

    Parte 2 concluida

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,7 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadoSerie;
+import br.com.alura.screenmatch.model.DadosEpisodios;
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
@@ -17,11 +18,15 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=481d1f0b");
 
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=481d1f0b");
 		ConverteDados converteDados = new ConverteDados();
-		DadoSerie dados = converteDados.obterDados(json, DadoSerie.class);
-		System.out.println(dados);
+		DadosSerie dadosS = converteDados.obterDados(json, DadosSerie.class);
+		System.out.println(dadosS);
+
+		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=481d1f0b");
+		DadosEpisodios dadosEp = converteDados.obterDados(json, DadosEpisodios.class);
+		System.out.println(dadosEp);
 
 	}
 }
