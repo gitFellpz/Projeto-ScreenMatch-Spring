990e244c8c2eda76cd746c37d11614796e716015
gitFellpz <felipe.adrilima@gmail.com>
2023-09-22 16:11:52 -0300

    Parte 1 concluida

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,27 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadoSerie;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=481d1f0b");
+
+		ConverteDados converteDados = new ConverteDados();
+		DadoSerie dados = converteDados.obterDados(json, DadoSerie.class);
+		System.out.println(dados);
+
+	}
 }
